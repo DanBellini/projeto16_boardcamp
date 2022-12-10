d12dd31598f187319a454238170babf07bc9fd6f
Daniel <danielbellinijf21@gmail.com>
2022-12-10 06:55:57 -0300

    function listCustormers

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -1,7 +1,27 @@
 import connection from "../database/db.js";
 
 async function listCustormers (req,res){
-
+    const {cpf} = req.query;
+    let findCustormers;
+    try {
+        if(cpf){
+            findCustormers = await connection.query(`
+            SELECT * FROM 
+                customers
+            WHERE
+                cpf
+            LIKE
+                $1;
+            `, [cpf + '%']);
+        } else {
+            findCustormers = await connection.query(`
+            SELECT * FROM customers;
+            `);
+        }
+        res.sendStatus(findCustormers.rows);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 };
 
 async function findCustormers (req,res){
