ebe1f59bf1d72071e3cfdc6d414ebb5c14bcf389
Daniel <danielbellinijf21@gmail.com>
2022-12-10 20:29:55 -0300

    function listRentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,7 +1,104 @@
 import connection from "../database/db.js";
 
 async function listRentals (req,res){
+    const {customersId, gameId} = req.query;
 
+    try {
+        if(customersId){
+            const listRentals = connection.query(`
+                SELECT
+                    rentals.*,
+                    customers.id
+                        AS
+                       "customersId",
+                    customers.name
+                        AS
+                        "customersName",
+                    games.id
+                        AS
+                        "gameId",
+                    games.name
+                        AS
+                        "gameName",
+                    games."categoryId",
+                    categories.name
+                        AS
+                        "categoryName"
+                FROM rentals
+                JOIN customers
+                    ON rentals."customerId" = customers.id
+                JOIN games
+                    ON rentals."gameId" = games.id
+                JOIN categories
+                    ON games."categoryId" = categories.id
+                    WHERE
+                    rentals."customersId" = $1;
+            `, [customersId]);
+            res.send(listRentals.rows);
+        }
+        if(gameId){
+            const listRentals = connection.query(`
+                SELECT
+                    rentals.*,
+                    customers.id
+                        AS
+                        "customersId",
+                    customers.name
+                        AS
+                        "customersName",
+                    games.id
+                        AS
+                        "gameId",
+                    games.name
+                        AS
+                        "gameName",
+                    games."categoryId",
+                    categories.name
+                        AS
+                        "categoryName"
+                FROM rentals
+                JOIN customers
+                    ON rentals."customerId" = customers.id
+                JOIN games
+                    ON rentals."gameId" = games.id
+                JOIN categories
+                    ON games."categoryId" = categories.id
+                    WHERE
+                    rentals."gameId" = $1;
+            `, [gameId]);
+            res.send(listRentals.rows)
+        };
+        const listRentals = connection.query(`
+            SELECT
+                rentals.*,
+                customers.id
+                    AS
+                   "customersId",
+                customers.name
+                    AS
+                    "customersName",
+                games.id
+                    AS
+                    "gameId",
+                games.name
+                    AS
+                    "gameName",
+                games."categoryId",
+                categories.name
+                    AS
+                    "categoryName"
+            FROM rentals
+            JOIN customers
+                ON rentals."customerId" = customers.id
+            JOIN games
+                ON rentals."gameId" = games.id
+            JOIN categories
+                ON games."categoryId" = categories.id;
+            `);
+        res.send(listRentals.rows);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 };
 
 async function insertIntoRentals (req,res){
