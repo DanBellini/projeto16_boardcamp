2b2bce525d41d58e323c798a5066abf27f5e0131
Daniel <danielbellinijf21@gmail.com>
2022-12-10 21:11:57 -0300

    function insertIntoRentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -1,4 +1,5 @@
 import connection from "../database/db.js";
+import dayjs from "dayjs";
 
 async function listRentals (req,res){
     const {customersId, gameId} = req.query;
@@ -66,7 +67,7 @@ async function listRentals (req,res){
                     WHERE
                     rentals."gameId" = $1;
             `, [gameId]);
-            res.send(listRentals.rows)
+            res.send(listRentals.rows);
         };
         const listRentals = connection.query(`
             SELECT
@@ -102,7 +103,43 @@ async function listRentals (req,res){
 };
 
 async function insertIntoRentals (req,res){
+    const {customersId, gameId, daysRented} = req.body;
 
+    try {
+        const idCustomers = await connection.query(`
+            SELECT * FROM customers WHERE id = $1;
+        `,[customersId]);
+
+        const idGame = await connection.query(`
+            SELECT * FROM customers WHERE id = $1;
+        `, [gameId]);
+
+        const rentDate = dayjs().format(YYYY-MM-DD)
+        
+        if(!idCustomers.rows.length || !idGame.rows.length || daysRented<=0) return res.sendStatus(400); 
+
+        const returnDate = null;
+
+        const pricePerDayGame = idGame.rows[0].pricePerDay * daysRented;
+
+        const delayfee = null;
+
+        await connection.query(`
+            INSERT INTO rentals (
+                "customersId",
+                "gameId",
+                "rentDate",
+                "daysRented",
+                "returnDate",
+                "originalPrice",
+                "delayFee")
+            VALUES ($1,$2,$3,$4,$5,$6,$7);
+        `,[customersId, gameId, rentDate, daysRented, returnDate, pricePerDayGame, delayfee]);
+
+        res.sendStatus(201);
+    } catch (error) {
+        res.sendStatus(500);
+    }
 };
 
 async function finishRentals (req,res){
